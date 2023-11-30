4778ab4edbc77a991b53a963f8b2440b01707822
nycollassobolevski <sobolevski.academy@gmail.com>
2023-11-30 17:16:03 -0300

    'sync'

--- front/App/Services/geoapify.js
+++ front/App/Services/geoapify.js
@@ -8,7 +8,7 @@ export default class GeoapifyService{
     }
     static getCoordsByString = async (urlEncoded) => {
         const url = `https://api.geoapify.com/v1/geocode/search?text=${urlEncoded}&apiKey=${GEOAPIFY_TOKEN}`;
-        const res = await axios.get(url)
-        return res
+        const res = await axios.get(url);
+        return res;
     }
 }
